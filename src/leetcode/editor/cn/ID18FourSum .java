69a02f2becea156de160916ed3e715121c8508c1
Xianmin Mo <xianminmo@stu.xjtu.edu.cn>
2024-08-24 13:44:03 +0800

    Dorm commit

---
+++ src/leetcode/editor/cn/ID18FourSum .java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+public class ID18FourSum{
+	public static void main(String[] args) {
+		Solution solution = new ID18FourSum().new Solution();
+		StringBuilder sb = new StringBuilder();
+		
+		//执行测试
+		
+		System.out.println(sb);		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
