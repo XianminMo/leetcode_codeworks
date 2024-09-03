6d313831b18c2b6866cd6892798d993710d5c246
Xianmin Mo <xianminmo@stu.xjtu.edu.cn>
2024-09-03 13:10:48 +0800

    shit

---
+++ src/leetcode/editor/cn/ID102BinaryTreeLevelOrderTraversal .java
@@ -0,0 +1,37 @@
+package leetcode.editor.cn;
+
+import javax.swing.tree.TreeNode;
+import java.util.*;
+public class ID102BinaryTreeLevelOrderTraversal{
+	public static void main(String[] args) {
+		Solution solution = new ID102BinaryTreeLevelOrderTraversal().new Solution();
+		StringBuilder sb = new StringBuilder();
+		
+		//执行测试
+		
+		System.out.println(sb);		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
